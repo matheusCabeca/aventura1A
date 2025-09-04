738206fd20efd19de4c1470f1701ad83f81b67ca
matheusCabeca <silva.duarte.matheus@escola.pr.gov.br>
2025-09-04 09:05:58 -0300

    ok

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const reiniciarBnt = document.getElementById('.btn-reiniciar');
 
 avanca.forEach(button =>{
 button.addEventListener('click',function(){
-const atual = document.querySelector('.ativo');]
-conts proximoPasso ='passo-'+this.getAttribute('data-proximo')    
+const atual = document.querySelector('.ativo');
+const proximoPasso ='passo-'+this.getAttribute('data-proximo')    
 
 
 
