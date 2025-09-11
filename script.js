0c749b3ffae3edca74cf1036b2ecf03825dfd469
matheusCabeca <silva.duarte.matheus@escola.pr.gov.br>
2025-09-11 08:45:29 -0300

    ok

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ const reiniciarBnt = document.getElementById('.btn-reiniciar');
 avanca.forEach(button =>{
 button.addEventListener('click',function(){
 const atual = document.querySelector('.ativo');
-const proximoPasso ='passo-'+this.getAttribute('data-proximo')    
+const proximoPasso ='passo-'+this.getAttribute('data-proximo')
+atual.classList('ativo');
 
 
 
+if (proximoElemento){
+    proximoElemento.classList.add('ativo');
+   }else{
+console.error(`Elemento com ID "${proximoPasso}"não encontrado.`)
+   }
 
-})
-
-
-}
-
-
-)
+   })
+});
